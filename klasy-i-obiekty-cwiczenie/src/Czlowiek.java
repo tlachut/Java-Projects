a57cf5a5d09436247cbc5e1ea2d08b05eb1f3240
Tomasz Łachut <tomus.lachut@gmail.com>
2024-02-17 23:18:36 +0100

    Dodano klasę Czlowiek

---
+++ klasy-i-obiekty-cwiczenie/src/Czlowiek.java
@@ -0,0 +1,22 @@
+public class Czlowiek {
+    // Cechy
+    int wiek;
+    String imie;
+    float waga;
+
+    // Metody
+    public void powiedzWiek() {
+        if (wiek >= 18) {
+            System.out.println("Mam " + wiek + " lat i jestem pełnoletni.");
+        } else {
+            System.out.println("Mam " + wiek + " lat i jestem niepełnoletni.");
+        }
+    }
+
+    public void przeliterujSwojeImie() {
+        for (int i = 0; i >= imie.length(); i++) {
+            System.out.println(imie.charAt(i));
+        }
+    }
+
+}
